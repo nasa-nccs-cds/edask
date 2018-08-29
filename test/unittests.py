c7061f31d5e52ee78b93858d92da2c19eeb19343
Thomas Maxwell <thomas.maxwell@nasa.gov>
2018-08-29 18:35:42 -0400

    Add xarray decycle

--- test/unittests.py
+++ test/unittests.py
@@ -114,21 +114,21 @@ class TestEdask(EDaskTestCase):
         results = self.mgr.testExec( domains, variables, operations )
         self.mgr.print(results)
 
-    def test_ave3(self):
-        domains = [{ "name":"d0",   "lat":  { "start":0, "end":10,  "system":"values" },
-                                    "lon":  { "start":100, "end":110, "system":"values" },
-                                    "time": { "start":'1980-01-01T00:00:00', "end":'1982-01-30T23:00:00', "system":"values"  } } ]
-        variables = [ { "uri": self.mgr.getAddress( "merra2", "tas"), "name":"tas:v0", "domain":"d0" } ]
-        operations = [ { "name":"xarray.ave", "input":"v0", "domain":"d0", "axes":"t", "resample": "t.season" } ]
-        results = self.mgr.testExec( domains, variables, operations )
-        self.mgr.print(results)
-
     def test_decycle(self):
         domains = [{ "name":"d0",   "lat":  { "start":0, "end":30,  "system":"values" },
                                     "lon":  { "start":100, "end":130, "system":"values" },
-                                    "time": { "start":'1980-01-01T00:00:00', "end":'2000-01-30T23:00:00', "system":"values"  } } ]
+                                    "time": { "start":'1980-01-01T00:00:00', "end":'1986-01-30T23:00:00', "system":"values"  } } ]
         variables = [ { "uri": self.mgr.getAddress( "merra2", "tas"), "name":"tas:v0", "domain":"d0" } ]
         operations = [ { "name":"xarray.decycle", "input":"v0" } ]
+        results = self.mgr.testExec( domains, variables, operations )
+        self.mgr.print(results)
+
+    def test_ave3(self):
+        domains = [{ "name":"d0",   "lat":  { "start":0, "end":30,  "system":"values" },
+                                    "lon":  { "start":100, "end":130, "system":"values" },
+                                    "time": { "start":'1980-01-01T00:00:00', "end":'1986-01-30T23:00:00', "system":"values"  } } ]
+        variables = [ { "uri": self.mgr.getAddress( "merra2", "tas"), "name":"tas:v0", "domain":"d0" } ]
+        operations = [ { "name":"xarray.ave", "input":"v0", "domain":"d0", "axes":"t", "resample": "t.season" } ]
         results = self.mgr.testExec( domains, variables, operations )
         self.mgr.print(results)
 
