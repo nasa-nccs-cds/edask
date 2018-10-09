fff237df68cdf88223320209f410eb45d20012e5
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2018-10-09 12:47:48 -0400

    Add sandbox.

---
+++ sandbox/binningTest.py
@@ -0,0 +1,17 @@
+from edask.process.test import TestManager
+import xarray as xr
+from xarray.core.resample import DatasetResample
+
+testMgr = TestManager("TESTS","binning")
+
+domains = [{"name": "d0", "lat": {"start": 0, "end": 10, "system": "values"},
+            "lon": {"start": 0, "end": 10, "system": "values"},
+            "time": {"start": '1980-01-01T00', "end": '1989-01-01T00', "system": "values"}}]
+variables = [{"uri": testMgr.getAddress("merra2", "tas"), "name": "tas:v0", "domain": "d0"}]
+operations = [{"name": "xarray.subset", "input": "v0"}]
+results = testMgr.testExec(domains, variables, operations)
+ds = results[0].xr
+# taxis = results[0].inputs[0].data.coords.get("t")
+# print( results[0].inputs[0].data.coords.get("t").dt )
+rs: DatasetResample = ds.resample(t='Q-FEB')
+print( rs.groups )
---
+++ sandbox/distributedTest.py
@@ -0,0 +1,52 @@
+import dask
+from dask.distributed import Client
+from typing import List, Dict, Sequence, Mapping, Any
+import xarray as xa
+import time, traceback, logging
+import numpy as np
+from edask.workflow.modules.xarray import *
+from edask.process.operation import WorkflowNode
+from edask.workflow.module import edasOpManager
+from edask.portal.parsers import WpsCwtParser
+from edask.workflow.kernel import Kernel
+
+if __name__ == '__main__':
+    logger = logging.getLogger()
+    logger.info( "STARTUP" )
+    dataset_path = '/Users/tpmaxwel/Dropbox/Tom/Data/GISS/CMIP5/E2H/r1i1p1/*.nc'
+    dataset_ncml = '/Users/tpmaxwel/.edas/cache/collections/agg/giss_r1i1p1-tas_Amon_GISS-E2-H_historical_r1i1p1_1.ncml'
+
+    testStr = '[ domain=[ {"name":"d0",   \n   "lat":{"start":0.0,  "end":20.0, "system":"values" }, "lon":{ "start":0.0,"end":20.0, "system":"values" }, "time":{ "start":0,"end":20, "system":"indices" } } ], ' \
+              'variable=[{ "collection":"cip_merra2_mon_1980-2015", "name":"tas:v0", "domain":"d0" } ], ' \
+              'operation=[{ "name":"xarray.ave", "input":"v0", "domain":"d0","axes":"xy"}] ]'
+
+    try:
+        tstart = time.time()
+        client = Client()
+
+        tdefine = time.time()
+        logger.info("Defining workflow")
+
+        def get_results( ) -> List[xa.Dataset]:
+            dataInputs = WpsCwtParser.parseDatainputs( testStr )
+            request: TaskRequest = TaskRequest.new( "requestId", "jobId", dataInputs )
+            return edasOpManager.buildRequest( request )
+
+        tsubmit = time.time()
+        result_future = client.submit( get_results )
+        logger.info("Submitted computation")
+        results: List[EDASDataset] = result_future.result()
+
+        logger.info( "\n Completed computation in {} seconds, workflow setup time = {}, cluster startup time = {}\n".format( str(time.time() - tsubmit), str(tsubmit - tdefine), str(tdefine - tstart) ) )
+
+        for result in results:
+            for variable in result.inputs:
+                logger.info( "\n Result: ")
+                logger.info( variable )
+
+
+    except Exception:
+        traceback.print_exc()
+
+    finally:
+        logger.info( "SHUTDOWN" )
