f8898d76bb7b14b503d37f833857401e40c6b8d6
Thomas Maxwell <thomas.maxwell@nasa.gov>
2018-10-24 15:29:24 -0400

    Update config

--- bin/startup_cluster.sh
+++ bin/startup_cluster.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ $# -eq 0 ]
   then
-    dask-ssh --hostfile $DIR/../resources/hosts $PUBLICKEY_OPTS
+    dask-ssh --hostfile $DIR/../resources/hosts $PKEY_OPTS
   else
-    dask-ssh --nprocs $1 --hostfile $DIR/../resources/hosts $PUBLICKEY_OPTS
+    dask-ssh --nprocs $1 --hostfile $DIR/../resources/hosts $PKEY_OPTS
 fi
