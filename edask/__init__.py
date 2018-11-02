b3314b57f854c6050e25085c3417e08d7c427c37
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2018-11-02 14:54:09 -0400

    Update dist logging

--- edask/__init__.py
+++ edask/__init__.py
@@ -1,10 +1,10 @@
-import logging, os, time
-LOG_DIR = os.path.expanduser("~/.edask/logs")
+import logging, os, time, socket
+LOG_DIR = os.path.expanduser( "~/.edask/logs" )
 if not os.path.exists(LOG_DIR):  os.makedirs(LOG_DIR)
 timestamp = time.strftime("%Y-%m-%d_%H:%M:%S", time.gmtime() )
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler( "{}/edask-{}.log".format( LOG_DIR, timestamp ) )
+fh = logging.FileHandler( "{}/edask-{}-{}.log".format( LOG_DIR, socket.gethostname(), timestamp ) )
 fh.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
