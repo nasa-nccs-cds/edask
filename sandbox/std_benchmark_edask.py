17a6cd99186563d0d37162d9bfbabd2deb1bf1a5
Thomas Maxwell <thomas.maxwell@nasa.gov>
2018-11-06 15:47:55 -0500

    Update tests

--- sandbox/std_benchmark_edask.py
+++ sandbox/std_benchmark_edask.py
@@ -10,7 +10,7 @@ start = time.time()
 
 edask_cip_tas_6hr="/dass/dassnsd/data01/cldra/data/pubrepo/CREATE-IP/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/6hr/atmos/tas/*.nc"
 edask_cip_tas_mon="/dass/dassnsd/data01/cldra/data/pubrepo/CREATE-IP/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/mon/atmos/tas/*.nc"
-dataset = edask_cip_tas_6hr
+dataset = edask_cip_tas_mon
 
 try:
     client = Client( 'edaskwndev01:8786' )
