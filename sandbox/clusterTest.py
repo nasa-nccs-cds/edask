8f9c51058040d1e033da8a3ecad6d4f0097d194a
Thomas Maxwell <thomas.maxwell@nasa.gov>
2018-11-06 15:25:01 -0500

    Update tests

--- sandbox/clusterTest.py
+++ sandbox/clusterTest.py
@@ -19,22 +19,27 @@ class ClusterTests:
         self.logger.info(result)
 
     def compute_eofs_TN(self):
+        t0 = time.time()
         domains = [{"name": "d0", "lat": {"start": -80, "end": 80, "system": "values"},  "time": {"start": '1880-01-01T00', "end": '2012-01-01T00', "system": "values"} }]
         variables = [{"uri": "collection:cip_20crv2c_mth", "name": "ts:v0", "domain": "d0"}]
         operations = [  {"name": "xarray.decycle", "axis":"t", "input": "v0", "norm":"true", "result":"dc"},
                         {"name": "xarray.detrend", "axis": "t", "input": "dc", "wsize": 50, "result": "dt"},
                         {"name": "xarray.eof", "modes": 4, "input": "dt", "archive":"eofs-20crv-ts-TN"  } ]
         result: EDASDataset = self.mgr.testExec(domains, variables, operations)
-        print( "Submitted Request, result = " + str( result ) )
+        print( "Submitted Request, elapsed: {} sec, result:".format( time.time()-t0 ) )
+        self.print(result)
 
     def compute_eofs_SN(self):
+        t0 = time.time()
         domains = [{"name": "d0", "lat": {"start": -80, "end": 80, "system": "values"},  "time": {"start": '1880-01-01T00', "end": '2012-01-01T00', "system": "values"} }]
         variables = [{"uri": "collection:cip_20crv2c_mth", "name": "ts:v0", "domain": "d0"}]
         operations = [  {"name": "xarray.decycle", "axis":"t", "input": "v0", "norm":"true", "result":"dc"},
                         {"name": "xarray.norm", "axis":"xy", "input": "dc", "result":"dt" },
                         {"name": "xarray.eof", "modes": 4, "input": "dt", "archive":"eofs-20crv-ts-SN" } ]
         result: EDASDataset = self.mgr.testExec(domains, variables, operations)
-        print( "Submitted Request, result = " + str( result ) )
+        print( "Submitted Request, elapsed: {} sec, result:".format( time.time()-t0 ) )
+        self.print(result)
+
 
     def test_subset(self):
         t0 = time.time()
@@ -44,16 +49,21 @@ class ClusterTests:
         variables = [{"uri": "collection:cip_merra2_mth", "name": "tas:v0", "domain": "d0"}]
         operations = [ { "name": "xarray.subset", "input": "v0" } ]
         result: EDASDataset = self.mgr.testExec(domains, variables, operations)
-        print( "Submitted Request, elapsed: {} sec, result = {}".format( time.time()-t0, str( result ) ) )
+        print( "Submitted Request, elapsed: {} sec, result:".format( time.time()-t0 ) )
+        self.print(result)
+
 
     def test_subset_dap(self):
+        t0 = time.time()
         domains = [{ "name":"d0",   "lat":  { "start":50, "end":55, "system":"values" },
                                     "lon":  { "start":40, "end":42, "system":"values" },
                                     "time": { "start":10, "end":15, "system":"indices" } } ]
         variables = [ { "uri": self.mgr.getAddress( "merra2", "tas"), "name":"tas:v0", "domain":"d0"  } ]
         operations = [ { "name":"xarray.subset", "input":"v0" } ]
         result: EDASDataset = self.mgr.testExec(domains, variables, operations)
-        print( "Submitted Request, result = " + str( result ) )
+        print( "Submitted Request, elapsed: {} sec, result:".format( time.time()-t0 ) )
+        self.print(result)
+
 
     def test_average(self):
         t0 = time.time()
@@ -62,7 +72,7 @@ class ClusterTests:
         operations = [ {"name": "xarray.ave", "input": "v0", "axis": "xy" } ]
         result: EDASDataset = self.mgr.testExec(domains, variables, operations)
         print( "Submitted Request, elapsed: {} sec, result:".format( time.time()-t0 ) )
-        for array in result.arrays: print( "\n" + str( array.xr.to_netcdf() ) )
+        self.print(result)
 
 if __name__ == '__main__':
     tester = ClusterTests()
