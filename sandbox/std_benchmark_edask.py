86fde156ac4b9c9044f71c4b1bd95cda331a2d7e
Thomas Maxwell <thomas.maxwell@nasa.gov>
2018-11-06 16:03:03 -0500

    Update tests

--- sandbox/std_benchmark_edask.py
+++ sandbox/std_benchmark_edask.py
@@ -2,23 +2,18 @@ import numpy as np
 import pandas as pd
 import xarray as xa
 import time, traceback
-from dask.distributed import Client, LocalCluster
-from multiprocessing import freeze_support
+from dask.distributed import Client
 
-freeze_support()
 print ( "STARTUP" )
 client = None
 start = time.time()
-nWorkers=8
 
 edask_cip_tas_6hr="/dass/dassnsd/data01/cldra/data/pubrepo/CREATE-IP/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/6hr/atmos/tas/*.nc"
 edask_cip_tas_mon="/dass/dassnsd/data01/cldra/data/pubrepo/CREATE-IP/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/mon/atmos/tas/*.nc"
 dataset = edask_cip_tas_mon
 
 try:
-#    client = Client( 'edaskwndev01:8786' )
-    client = Client( LocalCluster( n_workers=nWorkers ) )
-
+    client = Client( 'edaskwndev01:8786' )
     print( "READ " + dataset )
     
     ds_m=xa.open_mfdataset( dataset, autoclose=True, data_vars=['tas'], parallel=True )
