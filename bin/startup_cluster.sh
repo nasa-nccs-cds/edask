98e9a2e090c630577491515a6e2865a3024e3bf6
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-02-08 15:00:28 -0500

    Add custom scheduler and cluster handlers.

--- bin/startup_cluster.sh
+++ bin/startup_cluster.sh
@@ -8,7 +8,7 @@ while IFS=$': \t' read -a line ;do
 
 if [ $# -eq 0 ]
   then
-    $DIR/edask-ssh --hostfile $DIR/../resources/hosts --scheduler $myip:8786
+    $DIR/edask-ssh --hostfile $DIR/../resources/hosts --scheduler $myip:8786 $PKEY_OPTS
   else
-    $DIR/edask-ssh --nprocs $1 --hostfile $DIR/../resources/hosts --scheduler $myip:8786
+    $DIR/edask-ssh --nprocs $1 --hostfile $DIR/../resources/hosts --scheduler $myip:8786 $PKEY_OPTS
 fi
