571e55c4f10ab122e9fb195d9551cba1b60aae73
Thomas Maxwell <thomas.maxwell@nasa.gov>
2019-02-21 14:24:24 -0500

    Update zmq test.

---
+++ edas/stratus/test/zeromq/client/zmq_client.py
@@ -0,0 +1,25 @@
+from stratus.handlers.manager import handlers
+from stratus_endpoint.handler.base import Task, Status
+from edas.process.test import TestDataManager as mgr
+from stratus.handlers.app import StratusCore
+import os
+HERE = os.path.dirname(os.path.abspath(__file__))
+SETTINGS_FILE = os.path.join( HERE, "zmq_client_settings.ini" )
+
+if __name__ == "__main__":
+
+    stratus = StratusCore( settings=SETTINGS_FILE )
+
+    client = handlers.getClient( "zeromq" )
+
+    request = dict(
+        domain=[{"name": "d0", "lat": {"start": 50, "end": 55, "system": "values"},
+                 "lon": {"start": 40, "end": 42, "system": "values"},
+                 "time": {"start": "1980-01-01", "end": "1981-12-31", "crs": "timestamps"}}],
+        input=[{"uri": mgr.getAddress("merra2", "tas"), "name": "tas:v0", "domain": "d0"}],
+        operation=[{"name": "xarray.subset", "input": "v0"}]
+    )
+
+    task: Task = client.request( "exe", **request )
+    result = task.getResult( block=True )
+    print("Received result: " + str(result))
--- edas/stratus/test/zeromq/server/app_edas_endpoint.py
+++ edas/stratus/test/zeromq/server/app_edas_endpoint.py
@@ -1,7 +1,7 @@
 from stratus.handlers.zeromq.app import StratusApp
 import os
 HERE = os.path.dirname(os.path.abspath(__file__))
-SETTINGS_FILE = os.path.join( HERE, "edas_test_settings.ini" )
+SETTINGS_FILE = os.path.join( HERE, "edas_zmq_settings.ini" )
 
 if __name__ == '__main__':
     app = StratusApp( settings=SETTINGS_FILE )
