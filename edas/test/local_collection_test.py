a592e226080b923f053ebc41774d40a8d8252a2d
Thomas Maxwell <thomas.maxwell@nasa.gov>
2019-04-16 16:29:36 -0400

    Update collection test.

--- edas/test/local_collection_test.py
+++ edas/test/local_collection_test.py
@@ -6,7 +6,7 @@ def test_ave_timeslice():
     domains = [{ "name":"d0",   "lat":  { "start":-80, "end":80,  "system":"values" },
                                 "time": { "start":'1980-01-01T00Z', "end":'2010-01-01T00Z', "system":"timestamps"  } } ]
     variables = [ { "uri": mgr.getAddress( "merra2", "tas"), "name":"tas:v0", "domain":"d0" } ]
-    operations = [ { "name":"edas.ave", "input":"v0", "axes":"xy" } ]
+    operations = [ { "name":"xarray.ave", "input":"v0", "axes":"xy" } ]
     results = mgr.testExec( domains, variables, operations )
     mgr.print(results)
 
