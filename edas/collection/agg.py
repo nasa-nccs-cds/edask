7f22cd0bf91458d52b2edb629c5cc40ac3a10241
Thomas Maxwell <thomas.maxwell@nasa.gov>
2019-04-16 17:46:40 -0400

    Update periodPathList.

--- edas/collection/agg.py
+++ edas/collection/agg.py
@@ -292,7 +292,7 @@ class Aggregation:
             if file.date >= start:
                 if (len(paths) == 0) and (prev_file is not None):
                     paths.append( prev_file.getPath() )
-                    self.logger.info(f"@PPL: Adding path for date {file.date}: {file.getPath()}")
+                    self.logger.info(f"@PPL: Adding path for date {prev_file.date}: {prev_file.getPath()}")
                 paths.append( file.getPath() )
                 self.logger.info(f"@PPL: Adding path for date {file.date}: {file.getPath()}")
             prev_file = file
