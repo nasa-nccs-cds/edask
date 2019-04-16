d6e2e8a72f472e7eed5c99d4c8da6e3b9022b543
Thomas Maxwell <thomas.maxwell@nasa.gov>
2019-04-16 16:23:27 -0400

    Add collection test.

---
+++ edas/test/local_collection_test.py
@@ -0,0 +1,14 @@
+from edas.process.test import LocalTestManager
+appConf = { "sources.allowed": "collection,https" }
+mgr = LocalTestManager( "PyTest", "test_suite", appConf )
+
+def test_ave_timeslice():
+    domains = [{ "name":"d0",   "lat":  { "start":-80, "end":80,  "system":"values" },
+                                "time": { "start":'1980-01-01T00Z', "end":'2010-01-01T00Z', "system":"timestamps"  } } ]
+    variables = [ { "uri": mgr.getAddress( "merra2", "tas"), "name":"tas:v0", "domain":"d0" } ]
+    operations = [ { "name":"edas.ave", "input":"v0", "axes":"xy" } ]
+    results = mgr.testExec( domains, variables, operations )
+    mgr.print(results)
+
+if __name__ == "__main__":
+    test_ave_timeslice()
