4a10e95620b2be2af6e03e943cebeb78b57eb086
Thomas Maxwell <thomas.maxwell@nasa.gov>
2019-04-17 18:08:09 -0400

    Update tests

--- edas/test/collection_tests.py
+++ edas/test/collection_tests.py
@@ -21,6 +21,14 @@ def test_time_ave():
 
 def test_collection_time_ave(collection,variable,time_range):
     print( f"Executing Time average on var {variable} in collection {collection}, time range = {time_range}")
+    domains = [{ "name":"d0",  "time": {"start": time_range[0], "end": time_range[1], "crs": "timestamps"}  } ]
+    variables = [ { "uri": f"collection://{collection}:", "name":f"{variable}:v0", "domain":"d0" } ]
+    operations = [ { "name":"xarray.ave", "input":"v0", "axes":"t" } ]
+    results = mgr.testExec( domains, variables, operations )
+    mgr.print(results)
+
+def test_asia_time_ave(collection,variable,time_range):
+    print( f"Executing Asia Time average on var {variable} in collection {collection}, time range = {time_range}")
     domains = [{ "name":"d0",   "lat": {"start": 30, "end": 66, "system": "values"}, "lon": {"start": 45, "end": 135, "system": "values"},
                                 "time": {"start": time_range[0], "end": time_range[1], "crs": "timestamps"}  } ]
     variables = [ { "uri": f"collection://{collection}:", "name":f"{variable}:v0", "domain":"d0" } ]
@@ -30,10 +38,13 @@ def test_collection_time_ave(collection,variable,time_range):
 
 if __name__ == "__main__":
     if not LOCAL_TESTS: time.sleep(30)      # Allow cluster to startup
-    # collection = "merra2.m2t1nxlnd"
-    # variable = "LWLAND"
-    # time_range_10y = [ "1980-01-01", "1990-01-01" ]
-    # time_range_5m =  [ "1980-01-01", "1980-01-05"]
-    # test_collection_time_ave( collection, variable, time_range_5m )
-    test_time_ave()
+#    collection = "merra2.m2t1nxlnd"
+#    variable = "LWLAND"
+    collection = "cip_merra2_mth"
+    variable = "tas"
+    time_range_30y = [ "1980-01-01", "2010-01-01"]
+    time_range_10y = [ "1980-01-01", "1990-01-01" ]
+    time_range_5m =  [ "1980-01-01", "1980-01-05"]
+    test_collection_time_ave( collection, variable, time_range_30y )
+
 
