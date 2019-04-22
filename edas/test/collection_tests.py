4a65dee9e611cdc53002423f831c589140800b1c
Thomas Maxwell <thomas.maxwell@nasa.gov>
2019-04-22 12:20:16 -0400

    Update tests

--- edas/test/collection_tests.py
+++ edas/test/collection_tests.py
@@ -40,10 +40,10 @@ if __name__ == "__main__":
 #    collection = "merra2.m2t1nxlnd"
 #    variable = "LWLAND"
     collection = "cip_merra2_mth"
-    variable = "ta"
+    variable = "tas"
     time_range_30y = [ "1980-01-01", "2010-01-01"]
     time_range_10y = [ "1980-01-01", "1990-01-01" ]
     time_range_5m =  [ "1980-01-01", "1980-01-05"]
-    test_collection_time_ave( collection, variable, time_range_30y )
+    test_collection_time_ave( collection, variable, time_range_5m )
 
 
