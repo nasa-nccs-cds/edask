50a67a2190c2911674df1d57fa33fbb05e701b4f
Thomas Maxwell <thomas.maxwell@nasa.gov>
2019-04-24 18:31:26 -0400

    Update startup script

--- bin/startup_cluster_dask.sh
+++ bin/startup_cluster_dask.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ $# -eq 0 ]
   then
-    dask-ssh --hostfile ~/.edas/conf/hosts $PKEY_OPTS
+    dask-ssh --hostfile $HOME/.edas/conf/hosts $PKEY_OPTS
   else
-    dask-ssh --nprocs $1 --hostfile  ~/.edas/conf/hosts $PKEY_OPTS
+    dask-ssh --nprocs $1 --hostfile  $HOME/.edas/conf/hosts $PKEY_OPTS
 fi
