a073b962bedfd09cec05f51ce54a7b8b16f015f5
Thomas Maxwell <thomas.maxwell@nasa.gov>
2019-04-24 18:35:02 -0400

    Update startup script

--- bin/startup_cluster_dask.sh
+++ bin/startup_cluster_dask.sh
@@ -6,3 +6,6 @@ if [ $# -eq 0 ]
   else
     dask-ssh --nprocs $1 --hostfile  $HOME/.edas/conf/hosts $PKEY_OPTS
 fi
+
+
+# mkdir -p /home/edaskdev/.edas/logs && /dass/dassnsd/data01/sys/edaskdev/anaconda3/envs/edask/bin/python -m distributed.cli.dask_worker 10.71.13.11:8786 --nthreads 0 --host edaskwndev13 &> /home/edaskdev/.edas/logs/dask_scheduler_edaskwndev13.lo
