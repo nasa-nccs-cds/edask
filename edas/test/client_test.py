ca80e3caef5b3757cc23acc743eb9f6382238d31
Thomas Maxwell <thomas.maxwell@nasa.gov>
2019-04-24 15:27:11 -0400

    Update client_test main

--- edas/test/client_test.py
+++ edas/test/client_test.py
@@ -2,5 +2,10 @@ from distributed import Client
 import time
 
 if __name__ == '__main__':
+    print( "Starting client")
     client = Client()
-    time.sleep(60)
+    scheduler_info = client.scheduler_info()
+    workers = scheduler_info.pop("workers")
+    print(" @@@@@@@ SCHEDULER INFO: " + str(scheduler_info ))
+    print(f" N Workers: {len(workers)} " )
+    time.sleep(30)
--- edas/test/collection_tests.py
+++ edas/test/collection_tests.py
@@ -2,9 +2,8 @@ from edas.process.test import LocalTestManager, DistributedTestManager
 import time
 appConf = { "sources.allowed": "collection,https", "log.metrics": "true"}
 LOCAL_TESTS = False
-mgr = LocalTestManager( "PyTest", __file__, appConf ) if LOCAL_TESTS else DistributedTestManager( "PyTest",  __file__, appConf )
 
-def test_ave_timeslice():
+def test_ave_timeslice(mgr):
     domains = [{ "name":"d0",   "lat":  { "start":-80, "end":80,  "system":"values" },
                                 "time": { "start":'1980-01-01T00Z', "end":'2010-01-01T00Z', "system":"timestamps"  } } ]
     variables = [ { "uri": mgr.getAddress( "merra2", "tas"), "name":"tas:v0", "domain":"d0" } ]
@@ -12,14 +11,14 @@ def test_ave_timeslice():
     results = mgr.testExec( domains, variables, operations )
     mgr.print(results)
 
-def test_time_ave():
+def test_time_ave(mgr):
     domains = [{ "name":"d0" } ]
     variables = [ { "uri": mgr.getAddress( "merra2", "tas"), "name":"tas:v0", "domain":"d0" } ]
     operations = [ { "name":"xarray.ave", "input":"v0", "axes":"xy" } ]
     results = mgr.testExec( domains, variables, operations )
     mgr.print(results)
 
-def test_collection_time_ave(collection,variable,time_range):
+def test_collection_time_ave(mgr,collection,variable,time_range):
     print( f"Executing Time average on var {variable} in collection {collection}, time range = {time_range}")
     domains = [{ "name":"d0",  "time": {"start": time_range[0], "end": time_range[1], "crs": "timestamps"}  } ]
     variables = [ { "uri": f"collection://{collection}:", "name":f"{variable}:v0", "domain":"d0" } ]
@@ -27,7 +26,7 @@ def test_collection_time_ave(collection,variable,time_range):
     results = mgr.testExec( domains, variables, operations )
     mgr.print(results)
 
-def test_asia_time_ave(collection,variable,time_range):
+def test_asia_time_ave(mgr, collection,variable,time_range):
     print( f"Executing Asia Time average on var {variable} in collection {collection}, time range = {time_range}")
     domains = [{ "name":"d0",   "lat": {"start": 30, "end": 66, "system": "values"}, "lon": {"start": 45, "end": 135, "system": "values"},
                                 "time": {"start": time_range[0], "end": time_range[1], "crs": "timestamps"}  } ]
@@ -37,6 +36,7 @@ def test_asia_time_ave(collection,variable,time_range):
     mgr.print(results)
 
 if __name__ == "__main__":
+    mgr = LocalTestManager( "PyTest", __file__, appConf ) if LOCAL_TESTS else DistributedTestManager( "PyTest",  __file__, appConf )
 #    collection = "merra2.m2t1nxlnd"
     collection = "cip_eraint_mth"
 #    variable = "LWLAND"
@@ -46,6 +46,6 @@ if __name__ == "__main__":
     time_range_10y = [ "1981-01-01", "1991-01-01" ]
     time_range_1y  = [ "1991-01-01", "1992-01-01"]
     time_range_6m =  [ "1981-01-01", "1981-01-06"]
-    test_collection_time_ave( collection, variable, time_range_30y )
+    test_collection_time_ave( mgr, collection, variable, time_range_30y )
 
 
