fad552e403031544b62b81a14c03f4365f73c448
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-04-25 16:13:42 -0400

    Update cluster startup script

--- bin/startup_cluster_dask.sh
+++ bin/startup_cluster_dask.sh
@@ -2,10 +2,11 @@
 
 if [ $# -eq 0 ]
   then
-    dask-ssh --hostfile $HOME/.edas/conf/hosts $PKEY_OPTS
+    dask-ssh --hostfile $HOME/.edas/conf/hosts --remote-dask-worker distributed.cli.dask_worker --log-directory $HOME/.edas/logs $PKEY_OPTS
   else
-    dask-ssh --nprocs $1 --hostfile  $HOME/.edas/conf/hosts $PKEY_OPTS
+    dask-ssh --nprocs $1 --hostfile  $HOME/.edas/conf/hosts --remote-dask-worker distributed.cli.dask_worker --log-directory $HOME/.edas/logs $PKEY_OPTS
 fi
+
 
 
 # mkdir -p /home/edaskdev/.edas/logs && /dass/dassnsd/data01/sys/edaskdev/anaconda3/envs/edask/bin/python -m distributed.cli.dask_worker 10.71.13.11:8786 --nthreads 0 --host edaskwndev13 &> /home/edaskdev/.edas/logs/dask_scheduler_edaskwndev13.lo
