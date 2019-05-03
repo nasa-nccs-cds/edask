83bb15ce9dd9b69654a83bd6fc60e190df4e2ca2
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-05-03 11:20:02 -0400

    Update tests & data read

--- edas/workflow/kernel.py
+++ edas/workflow/kernel.py
@@ -274,7 +274,7 @@ class InputKernel(Kernel):
                 dset = xr.open_mfdataset( [dataPath] )
                 self.importToDatasetCollection(results, request, snode, dset)
             elif dataSource.type == SourceType.dap:
-                nchunks = 8
+                nchunks = 16
                 self.logger.info( f" --------------->>> Reading data from address: {dataSource.address}, nchunks = {nchunks}" )
                 dset = xr.open_mfdataset([dataSource.address], engine="netcdf4", data_vars=snode.varSource.ids, chunks={"time":nchunks}  )
                 self.importToDatasetCollection( results, request, snode, dset )
