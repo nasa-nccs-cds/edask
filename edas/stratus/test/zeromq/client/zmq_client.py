3aaf5f3cb7059a6c0e7331bbbd22c642dbc11461
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-05-07 15:30:00 -0400

    Update test

--- edas/stratus/test/zeromq/client/zmq_client.py
+++ edas/stratus/test/zeromq/client/zmq_client.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
         input=[ dict( uri=uri, name=f"tas:v{i}", domain=f"d{i}" ) for i in range(numDomains) ],
 
-        operation=[ dict( name="edas.ave", axis="xy", input=f"v{i}" ) for i in range(numDomains) ]
+        operation=[ dict( name="edas:ave", axis="xy", input=f"v{i}" ) for i in range(numDomains) ]
     )
 
     task: TaskHandle = client.request( requestSpec )
