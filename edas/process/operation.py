548150a69059211b1157b90b7f51002a132a252c
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-05-19 12:55:38 -0400

    Update OpNode

--- edas/process/operation.py
+++ edas/process/operation.py
@@ -277,8 +277,11 @@ class OpNode(WorkflowNode):
         return OpNode(name, domain, operationSpec)
 
     def __init__(self, name: str, domain: str, metadata: Dict[str,Any] ):
-        super( OpNode, self ).__init__( name, domain, metadata)
-        self.logger.info( "Create OPNode: " + str(metadata) )
+        super( OpNode, self ).__init__( name, domain, self.filterMetadata(metadata) )
+        self.logger.info (" Create OpNode: " + str(self.metadata))
+
+    def filterMetadata(self, metadata: Dict[str,Any] ) -> Dict[str,Any]:
+        return { k: v for k, v in metadata.items() if v != "" }
 
 
     def isResult(self):
