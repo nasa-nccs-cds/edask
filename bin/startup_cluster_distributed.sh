a908ed7c576588aed39185b96751bacc4a167b74
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-06-14 15:47:11 -0400

    Config chunking

--- bin/startup_cluster_distributed.sh
+++ bin/startup_cluster_distributed.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-dask-ssh --nprocs 8 --hostfile  $HOME/.edas/conf/hosts --remote-dask-worker distributed.cli.dask_worker --log-directory /tmp --memory-limit auto $PKEY_OPTS
+dask-ssh --hostfile  $HOME/.edas/conf/hosts --remote-dask-worker distributed.cli.dask_worker --log-directory /tmp --memory-limit auto $PKEY_OPTS
 
 # PKEY Example:
 # PKEY_OPTS=--ssh-private-key=/home/edaskdev/.ssh/id_edaskdev
