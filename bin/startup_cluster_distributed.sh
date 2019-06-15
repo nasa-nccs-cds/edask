77c7708fffa1143459181ae5615a64a719d37dbf
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-06-15 13:18:41 -0400

    Update chunking

--- bin/startup_cluster_distributed.sh
+++ bin/startup_cluster_distributed.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
-dask-ssh --hostfile  $HOME/.edas/conf/hosts --remote-dask-worker distributed.cli.dask_worker --log-directory /tmp --memory-limit auto $PKEY_OPTS
+LOG_DIR=${EDASK_CACHE_DIR:=/tmp}
+dask-ssh --hostfile  $HOME/.edas/conf/hosts --remote-dask-worker distributed.cli.dask_worker --log-directory $LOG_DIR $PKEY_OPTS
 
 # PKEY Example:
 # PKEY_OPTS=--ssh-private-key=/home/edaskdev/.ssh/id_edaskdev
