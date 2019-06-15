090db1f17cebbfe4bf789656914dec53039daee7
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-06-14 20:23:15 -0400

    Update startup

--- bin/startup_cluster_distributed.sh
+++ bin/startup_cluster_distributed.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-dask-ssh --hostfile  $HOME/.edas/conf/hosts --remote-dask-worker distributed.cli.dask_worker --log-directory /tmp --memory-limit 0.75 $PKEY_OPTS
+dask-ssh --hostfile  $HOME/.edas/conf/hosts --remote-dask-worker distributed.cli.dask_worker --log-directory /tmp --memory-limit auto $PKEY_OPTS
 
 # PKEY Example:
 # PKEY_OPTS=--ssh-private-key=/home/edaskdev/.ssh/id_edaskdev
