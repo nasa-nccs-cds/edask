70b9284c6523b6cb59b45771e700ac23a9c88e7b
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-07-08 15:21:40 -0400

    Update tests

--- edas/test/collection_tests.py
+++ edas/test/collection_tests.py
@@ -7,14 +7,14 @@ def test_ave_timeslice(mgr):
     domains = [{ "name":"d0",   "lat":  { "start":-80, "end":80,  "system":"values" },
                                 "time": { "start":'1980-01-01T00Z', "end":'2010-01-01T00Z', "system":"timestamps"  } } ]
     variables = [ { "uri": mgr.getAddress( "merra2", "tas"), "name":"tas:v0", "domain":"d0" } ]
-    operations = [ { "name":"xarray.ave", "input":"v0", "axes":"xy" } ]
+    operations = [ { "name":"edas.ave", "input":"v0", "axes":"xy" } ]
     results = mgr.testExec( domains, variables, operations )
     mgr.print(results)
 
 def test_time_ave(mgr):
     domains = [{ "name":"d0" } ]
     variables = [ { "uri": mgr.getAddress( "merra2", "tas"), "name":"tas:v0", "domain":"d0" } ]
-    operations = [ { "name":"xarray.ave", "input":"v0", "axes":"xy" } ]
+    operations = [ { "name":"edas.ave", "input":"v0", "axes":"xy" } ]
     results = mgr.testExec( domains, variables, operations )
     mgr.print(results)
 
@@ -22,7 +22,7 @@ def test_collection_time_ave(mgr,collection,variable,time_range):
     print( f"Executing Time average on var {variable} in collection {collection}, time range = {time_range}")
     domains = [{ "name":"d0",  "time": {"start": time_range[0], "end": time_range[1], "crs": "timestamps"}  } ]
     variables = [ { "uri": f"collection://{collection}:", "name":f"{variable}:v0", "domain":"d0" } ]
-    operations = [ { "name":"xarray.ave", "input":"v0", "axes":"t" } ]
+    operations = [ { "name":"edas.ave", "input":"v0", "axes":"t" } ]
     results = mgr.testExec( domains, variables, operations )
     mgr.print(results)
 
@@ -30,7 +30,7 @@ def test_collection_mean(mgr,collection,variable,time_range):
     print( f"Executing Time average on var {variable} in collection {collection}, time range = {time_range}")
     domains = [{ "name":"d0",  "time": {"start": time_range[0], "end": time_range[1], "crs": "timestamps"}  } ]
     variables = [ { "uri": f"collection://{collection}:", "name":f"{variable}:v0", "domain":"d0" } ]
-    operations = [ { "name":"xarray.mean", "input":"v0", "axes":"tyx" } ]
+    operations = [ { "name":"edas.mean", "input":"v0", "axes":"tyx" } ]
     results = mgr.testExec( domains, variables, operations )
     mgr.print(results)
 
@@ -39,7 +39,7 @@ def test_asia_time_ave(mgr, collection,variable,time_range):
     domains = [{ "name":"d0",   "lat": {"start": 30, "end": 66, "system": "values"}, "lon": {"start": 45, "end": 135, "system": "values"},
                                 "time": {"start": time_range[0], "end": time_range[1], "crs": "timestamps"}  } ]
     variables = [ { "uri": f"collection://{collection}:", "name":f"{variable}:v0", "domain":"d0" } ]
-    operations = [ { "name":"xarray.ave", "input":"v0", "axes":"t" } ]
+    operations = [ { "name":"edas.ave", "input":"v0", "axes":"t" } ]
     results = mgr.testExec( domains, variables, operations )
     mgr.print(results)
 
