426dc96d311be8dab8f7f6550339b2325f80517a
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-07-10 15:45:20 -0400

    Update WorldClim Test

--- edas/test/world_clim_collection_test.py
+++ edas/test/world_clim_collection_test.py
@@ -6,24 +6,28 @@ appConf = { "sources.allowed": "collection,https" }
 LOCAL_TESTS = False
 GLOBAL = True
 
-def test_world_clim (mgr ):
+def test_world_clim_mean ( mgr, start_year ):
 
-    domains = [ {"name": "d0", "time": {"start": '1990-01-01T00Z', "end": '2000-01-01T00Z', "system": "timestamps"}, "lev": {"start": 0, "end": 0, "system": "indices"} } ]
-
-    if not GLOBAL:
-        domains[0]["lat"] = {"start": 25, "end": 50, "system": "values"}
-        domains[0]["lon"] = {"start": 200, "end": 300, "system": "values"}
-
-    variables = [{"uri": "collection://cip_merra2_6hr", "name": "tas:temp", "domain": "d0"},
-                 {"uri": "collection://cip_merra2_6hr", "name": "hus:moist", "domain": "d0"}]
-
+    variables = [{"uri": "collection://cip_merra2_6hr", "name": "tas:temp", "domain": "d0"}, {"uri": "collection://cip_merra2_6hr", "name": "hus:moist", "domain": "d0"}]
     operations = [{"name": "edas.worldClim", "input": "temp,moist"}]
 
-    results:  List[EDASDataset] = mgr.testExec( domains, variables, operations )
-    results[0].save( "cip_merra2_6hr-WorldClim")
+    worldClimResults = {}
+    for iYear in range(0,10):
+        base_year = start_year + iYear
+        domains = [ {"name": "d0", "time": {"start": f'{base_year}-01-01T00Z', "end": f'{base_year+1}-01-01T00Z', "system": "timestamps"}, "lev": {"start": 0, "end": 0, "system": "indices"} } ]
+        if not GLOBAL:
+            domains[0]["lat"] = {"start": 25, "end": 50, "system": "values"}
+            domains[0]["lon"] = {"start": 200, "end": 300, "system": "values"}
+        print( f"Computing WorldClim fields with domain {domains[0]}")
+        results:  List[EDASDataset] = mgr.testExec( domains, variables, operations )
+        worldClimResults[iYear] =  results[0]
+
+    worldClimResultSum = worldClimResults[0]
+    for iYear in range(1, 10): worldClimResultSum = worldClimResultSum + worldClimResults[iYear]
+    (worldClimResultSum/10).save( f"cip_merra2_6hr-WorldClim-{start_year}")
 
 if __name__ == "__main__":
     mgr = LocalTestManager( "PyTest", __file__, appConf ) if LOCAL_TESTS else DistributedTestManager( "PyTest",  __file__, { **appConf, "scheduler.address":"edaskwndev01:8786" } )
-    test_world_clim( mgr )
+    test_world_clim_mean( mgr, 1990 )
 
 
