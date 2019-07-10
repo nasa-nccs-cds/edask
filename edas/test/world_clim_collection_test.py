8dc7242b4129d969fdc20075155791580fee365f
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-07-10 17:00:52 -0400

    Update WorldClim test

--- edas/test/world_clim_collection_test.py
+++ edas/test/world_clim_collection_test.py
@@ -20,11 +20,12 @@ def test_world_clim_mean ( mgr, start_year ):
             domains[0]["lon"] = {"start": 200, "end": 300, "system": "values"}
         print( f"Computing WorldClim fields with domain {domains[0]}")
         results:  List[EDASDataset] = mgr.testExec( domains, variables, operations )
+        results[0].save(f"merra2-WorldClim-{base_year}")
         worldClimResults[iYear] =  results[0]
 
     worldClimResultSum = worldClimResults[0]
     for iYear in range(1, 10): worldClimResultSum = worldClimResultSum + worldClimResults[iYear]
-    (worldClimResultSum/10).save( f"cip_merra2_6hr-WorldClim-{start_year}")
+    (worldClimResultSum/10).save( f"merra2-WorldClim-mean-{start_year}-{start_year+10}")
 
 if __name__ == "__main__":
     mgr = LocalTestManager( "PyTest", __file__, appConf ) if LOCAL_TESTS else DistributedTestManager( "PyTest",  __file__, { **appConf, "scheduler.address":"edaskwndev01:8786" } )
