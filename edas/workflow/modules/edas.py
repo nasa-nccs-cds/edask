afa181b87fd17971df90c6d6284319e12e6355fa
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-07-11 15:26:50 -0400

    Add EDASDataset combine

--- edas/workflow/modules/edas.py
+++ edas/workflow/modules/edas.py
@@ -170,7 +170,7 @@ class WorldClimKernel(OpKernel):
 
 
     def processInputCrossSection( self, request: TaskRequest, node: OpNode, inputs: EDASDataset  ) -> EDASDataset:
-        self.logger.info( f"Computing WorldClim fields for domain: {request.domain( node.domain )}" )
+        self.logger.info( f"Computing WorldClim fields for domains: {list(request.operationManager.domains.domains.values())}" )
         resultArrays: Dict[str,EDASArray] = {  }
         tempID = node.getParm("temp","temp")
         version = node.getParm("version", "mean")
