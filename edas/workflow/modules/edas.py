2049875291e384f80ac3ddd1720525c441cfeaf2
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-07-17 15:37:17 -0400

    Add diag

--- edas/workflow/modules/edas.py
+++ edas/workflow/modules/edas.py
@@ -150,7 +150,7 @@ class WorldClimKernel(OpKernel):
     def getValueForSelectedQuarter(self, targetVar: Optional["EDASArray"], selectionVar: "EDASArray", op: str, name: str ) -> "EDASArray":
 #        self.logger.info( f" getValueForSelectedQuarter, dims = {selectionVar.xr.dims}")
         selectionData: xa.DataArray = selectionVar.xr.chunk({'m':3})
-        lowpassSelector: xa.DataArray = selectionData.rolling( m=3, min_periods=2, center=True ).mean()   # TODO: handle boundary conditions as in Spec
+        lowpassSelector: xa.DataArray = selectionData.rolling( m=3, min_periods=2, center=True ).mean()
         if op == "max":   selectedMonth: xa.DataArray = lowpassSelector.argmax( "m", keep_attrs=True )
         elif op == "min": selectedMonth: xa.DataArray = lowpassSelector.argmin( "m", keep_attrs=True )
         else: raise Exception( "Unrecognized operation in getValueForSelectedQuarter: " + op )
@@ -162,7 +162,8 @@ class WorldClimKernel(OpKernel):
             resultVar = selectionVar
         else:
             selectors = [ ( selectedMonth - 1 ) % 12, selectedMonth, (selectedMonth + 1) % 12 ]
-            target = self.stack( targetVar.xr)
+            target: xa.DataArray = self.stack( targetVar.xr)
+            self.logger.info( f"getValueForSelectedQuarter: Processing target var {target.name} with shape {target.shape}, op: {op}, selectionVar: {selectionVar.name}")
             targetVars = [ target.isel( m=self.stack( selector ) ) for selector in selectors ]
             resultXarray = ( targetVars[0] + targetVars[1] + targetVars[2] ) / 3
             resultVar = targetVar
