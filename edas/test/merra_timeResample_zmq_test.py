863fe9e5360e34a0a2212e28cfb39d941b53ffb5
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-07-20 13:11:25 -0400

    Update data processing script

--- edas/test/merra_timeResample_zmq_test.py
+++ edas/test/merra_timeResample_zmq_test.py
@@ -1,30 +1,59 @@
 from stratus_endpoint.handler.base import TaskHandle, TaskResult
 from typing import Sequence, List, Dict, Mapping, Optional, Any
 from edas.workflow.kernel import EDASDataset
-import xarray as xa
 from stratus.app.core import StratusCore
 
 settings = dict(stratus=dict(type="zeromq", client_address="127.0.0.1", request_port="4556", response_port="4557"))
 stratus = StratusCore(settings)
 client = stratus.getClient()
+
+collection = "cip_merra2_6hr"
+vnames = dict( temp='tas', precip='pr', humid='hur', clouds='clt' )
+
+singleYear = True
 base_year = 1990
 nYears = 1
 
-variables_1h = [{"uri": "collection://merra2_inst1_2d_asm_Nx", "name": "T2M:temp", "domain": "d0"},
-                {"uri": "collection://merra2_inst1_2d_asm_Nx", "name": "QV2M:moist", "domain": "d0"}]
+variables = [ dict( uri="collection://"+collection, name=vnames[type]+":"+type, domain="d0") for type in ['temp','precip','humid','clouds'] ]
 
-variables_6h = [{"uri": "collection://cip_merra2_6hr", "name": "tas:temp", "domain": "d0"},
-                {"uri": "collection://cip_merra2_6hr", "name": "pr:moist", "domain": "d0"}]
+domains0 = [ { "name": "d0" } ]
+domains1 = [ {"name": "d0", "time": {"start": f'{base_year}-01-01T00Z', "end": f'{base_year+nYears-1}-12-31T23Z', "system": "timestamps"} } ]
+domains = domains1 if singleYear else domains0
+
+def compute( requestSpec: Dict, rid: str ):
+    task: TaskHandle = client.request(requestSpec)
+    result: Optional[TaskResult] = task.getResult(block=True)
+    edasDataset = EDASDataset.new(result.getDataset())
+    edasDataset.save(rid)
+
+# Compute temperature products
 
 operations = [ { "name": "edas.timeResample", "input": "temp:daily_maxmin", "freq": "1D", "op": "max,min" },
                { "name": "edas.timeResample", "input": "daily_maxmin", "freq": "1M", "op": "ave,std" } ]
+requestSpec = dict( domain=domains, input = variables, operation = operations )
+compute( requestSpec, "merra2-temp-dailyMax-monthlyAve" )
 
-domains0 = [{ "name": "d0" }]
-domains1 = [{"name": "d0", "time": {"start": f'{base_year}-01-01T00Z', "end": f'{base_year+nYears-1}-12-31T23Z', "system": "timestamps"}}]
 
-requestSpec = dict( domain=domains1, input = variables_6h, operation = operations )
-task: TaskHandle = client.request(requestSpec)
-result: Optional[TaskResult] = task.getResult(block=True)
-edasDataset = EDASDataset.new( result.getDataset() )
-edasDataset.save( "cip-merra2-tas-dailyMax-monthlyAve" )
+# Compute precipitation products
+
+operations = [ { "name": "edas.timeResample", "input": "precip", "freq": "1M", "op": "sum,std" } ]
+requestSpec = dict( domain=domains, input = variables, operation = operations )
+compute( requestSpec, "merra2-precip-monthlySum" )
+
+
+# Compute relative humidity products
+
+operations = [ { "name": "edas.timeResample", "input": "humid", "freq": "1D", "op": "ave,std" } ]
+requestSpec = dict( domain=domains, input = variables, operation = operations )
+compute( requestSpec, "merra2-humid-dailyAve" )
+
+
+# Compute cloud cover products
+
+operations = [ { "name": "edas.timeResample", "input": "clouds", "freq": "1D", "op": "ave,std" } ]
+requestSpec = dict( domain=domains, input = variables, operation = operations )
+compute( requestSpec, "merra2-clouds-dailyAve" )
+
+
+
 
