f9b60e1836357f07a1ff4b2dd2b6a9fe15d980c5
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-10-03 13:27:14 -0400

    Fix rename

--- edas/workflow/data.py
+++ edas/workflow/data.py
@@ -492,12 +492,12 @@ class EDASDataset:
 
         for id,val in idMap.items():
             if val not in dataset and val not in dataset.dims:
-                dataset = dataset.rename( {id:val}, True )
+                dataset = dataset.rename( {id:val} )
             else:  skipMap[id] = val
 
         for id,val in skipMap.items():
             if val not in dataset and val not in dataset.dims:
-                dataset = dataset.rename( {id:val}, True )
+                dataset = dataset.rename( {id:val} )
 
         return dataset
 
@@ -517,7 +517,7 @@ class EDASDataset:
             domid: str = dataset.attrs.get("domid")
             for id,val in self.StandardAxisMap.items():
                 if id in dataset.dims and val not in dataset.dims:
-                    dataset = dataset.rename( {id:val}, True )
+                    dataset = dataset.rename( {id:val} )
             result_attrs = { **self.attrs, **new_attrs }
             results.append( self.fromXr( dataset, domid, result_attrs ) )
         return results
