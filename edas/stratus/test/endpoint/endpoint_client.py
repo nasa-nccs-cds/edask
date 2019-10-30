40daf7cc20e61792356476bac218e28f46cb6b22
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-10-30 17:29:26 -0400

    update slurm test

--- edas/stratus/test/endpoint/endpoint_client.py
+++ edas/stratus/test/endpoint/endpoint_client.py
@@ -2,7 +2,7 @@ from stratus_endpoint.handler.base import TaskHandle, TaskResult
 from typing import Sequence, List, Dict, Mapping, Optional, Any
 from edas.process.test import TestDataManager as mgr
 from stratus.app.client import StratusClient
-import logging
+import logging, os
 import xarray as xa
 from stratus.app.core import StratusCore
 from edas.config import EdasEnv
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     result: Optional[TaskResult] = task.getResult( block=True )
     dsets: List[xa.Dataset] = result.data
     for index,dset in enumerate(dsets):
-        fileName =  f"/tmp/edas_endpoint_test_result-{index}.nc"
+        fileName = os.path.expanduser(f"~/edas_endpoint_test_result-{index}.nc")
         print( f"Got result[{index}]: Saving to file {fileName} " )
         dset.to_netcdf( fileName )
         print(f"Completed saving dataset[{index}]: Variables = {dset.variables.keys()} ")
