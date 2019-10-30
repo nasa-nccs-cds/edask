56fe11120a13c8e700bb4c0e0dc3997009fe69af
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-10-30 16:34:54 -0400

    Add dask_jobqueue test

---
+++ edas/test/dask_jobqueue_test.py
@@ -0,0 +1,24 @@
+import logging, time
+import xarray as xr
+from dask.distributed import Client
+from typing import List, Optional, Tuple, Dict, Any
+from dask_jobqueue import SLURMCluster
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
+variable = "tas"
+uri = 'https://dataserver.nccs.nasa.gov/thredds/dodsC/bypass/CREATE-IP/reanalysis/MERRA2/mon/atmos/tas.ncml'
+
+cluster = SLURMCluster( queue="myNodes" )
+print( "CLUSTER JOB SCRIPT: " + cluster.job_script() )
+client = Client( cluster )
+
+dset: xr.Dataset = xr.open_dataset( uri )
+da: xr.DataArray = dset['tas']
+da2: xr.DataArray = da.groupby('time.month').mean('time')
+da_monthly = da2.load()
+print(da_monthly)
+
+client.close()
+cluster.close()
+
+
