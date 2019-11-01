7d5c572186ee522d076186dd01823b7e7c97be9a
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-11-01 13:35:59 -0400

    update slurm test

--- edas/stratus/test/endpoint/endpoint_client.py
+++ edas/stratus/test/endpoint/endpoint_client.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     dsets: List[xa.Dataset] = result.data
 
     print( dsets[0] )
-    print( dsets[0].values )
+    print( dsets[0].values() )
 
     # for index,dset in enumerate(dsets):
     #     fileName = os.path.expanduser(f"~/edas_endpoint_test_result-{index}.nc")
