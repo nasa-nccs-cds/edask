5af297308cde65c67fe978db8b019b720a85fcf1
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2019-11-01 13:20:57 -0400

    update slurm test

--- edas/stratus/test/endpoint/endpoint_client.py
+++ edas/stratus/test/endpoint/endpoint_client.py
@@ -12,29 +12,34 @@ if __name__ == "__main__":
 
     slurmScheduler = 'slurm:default'
     daskScheduler = 'explore101:8786'
-    appConf = { 'scheduler.address': daskScheduler }
+    appConf = { 'scheduler.address': slurmScheduler }
     EdasEnv.update( appConf )
+
+    variable = "tas"
+    uri = 'https://dataserver.nccs.nasa.gov/thredds/dodsC/bypass/CREATE-IP/reanalysis/MERRA2/mon/atmos/tas.ncml'
 
     settings = dict( stratus = dict( type="endpoint", module="edas.stratus.endpoint", object="EDASEndpoint" ) )
     stratus = StratusCore( settings )
     client: StratusClient = stratus.getClient()
     time_range = {"start": "1980-01-01", "end": "2001-12-31", "crs": "timestamps"}
-    uri =  mgr.getAddress("merra2", "ta")
 
     requestSpec = dict(
-        domain=[ dict(name="d0", time=time_range) ],
-        input=[ dict( uri=uri, name=f"ta:v0", domain=f"d0" ) ],
-        operation=[ dict( name="edas:ave", axis="xy", input=f"v0" )  ]
+        domain=[ dict(name="d0") ],
+        input=[ dict( uri=uri, name=f"{variable}:v0", domain=f"d0" ) ],
+        operation=[ dict( name="edas:ave", axis="t", input=f"v0" )  ]
     )
 
     task: TaskHandle = client.request( requestSpec )
     result: Optional[TaskResult] = task.getResult( block=True )
     dsets: List[xa.Dataset] = result.data
-    for index,dset in enumerate(dsets):
-        fileName = os.path.expanduser(f"~/edas_endpoint_test_result-{index}.nc")
-        print( f"Got result[{index}]: Saving to file {fileName} " )
-        dset.to_netcdf( fileName )
-        print(f"Completed saving dataset[{index}]: Variables = {dset.variables.keys()} ")
+
+    print( dsets[0] )
+
+    # for index,dset in enumerate(dsets):
+    #     fileName = os.path.expanduser(f"~/edas_endpoint_test_result-{index}.nc")
+    #     print( f"Got result[{index}]: Saving to file {fileName} " )
+    #     dset.to_netcdf( fileName )
+    #     print(f"Completed saving dataset[{index}]: Variables = {dset.variables.keys()} ")
 
     exit(0)
 
