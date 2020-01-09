6f60f0e077509175168fb2e1b04ac6eff2070475
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2020-01-09 14:56:29 -0500

    Update test

--- edas/test/merra_timeResample_slurm_test.py
+++ edas/test/merra_timeResample_slurm_test.py
@@ -3,7 +3,7 @@ from typing import Sequence, List, Dict, Mapping, Optional, Any
 import os, xarray as xr
 from stratus.app.core import StratusCore
 from edas.config import EdasEnv
-import traceback
+import traceback, time
 OUTPUT_DIR = os.path.expanduser("/att/pubrepo/ILAB/data/cache/results")
 os.makedirs(OUTPUT_DIR,exist_ok=True)
 appConf = {'scheduler.address': 'slurm:default' }
@@ -11,62 +11,61 @@ EdasEnv.update(appConf)
 
 settings = dict(stratus=dict(type="endpoint", module="edas.stratus.endpoint", object="EDASEndpoint"))
 stratus = StratusCore(settings)
-client = stratus.getClient()
 
-collection = "cip_merra2_6hr"
-vnames = dict( temp='tas', precip='pr', humid='hur', clouds='clt' )
+with stratus.getClient() as client:
 
-singleYear = True
-base_year = 2014
-nYears = 1
+    collection = "cip_merra2_6hr"
+    vnames = dict( temp='tas', precip='pr', humid='hur', clouds='clt' )
 
-variables = [ dict( uri=f"collection://{collection}", name=f"{vnames[vtype]}:{vtype}", domain="d0") for vtype in ['temp','precip','humid','clouds'] ]
+    singleYear = True
+    base_year = 2014
+    nYears = 1
 
-domains0 = [ { "name": "d0" } ]
-domains1 = [ {"name": "d0", "time": {"start": f'{base_year}-01-01T00Z', "end": f'{base_year+nYears-1}-12-31T23Z', "system": "timestamps"} } ]
-domains = domains1 if singleYear else domains0
+    variables = [ dict( uri=f"collection://{collection}", name=f"{vnames[vtype]}:{vtype}", domain="d0") for vtype in ['temp','precip','humid','clouds'] ]
 
-def compute( requestSpec: Dict, rid: str ):
-    try:
-        task: TaskHandle = client.request(requestSpec)
-        result: Optional[TaskResult] = task.getResult(block=True)
-        result_dataset = result.getDataset()
-        output_file = f"{OUTPUT_DIR}/{rid}.nc"
-        try:                          result_dataset.to_netcdf( output_file )
-        except Exception as err0:     print( f"\n\n   &&&&&&&&   ERROR writing result to output file: {output_file}: {err0}   &&&&&&&&   \n\n" )
-    except Exception as err1:
-        print(f"\n\n  &&&&&&&&   Execution ERROR : {err1}   &&&&&&&& ")
-        traceback.print_exc()
+    domains0 = [ { "name": "d0" } ]
+    domains1 = [ {"name": "d0", "time": {"start": f'{base_year}-01-01T00Z', "end": f'{base_year+nYears-1}-12-31T23Z', "system": "timestamps"} } ]
+    domains = domains1 if singleYear else domains0
 
-print( "Compute temperature products ------------------------------------------------------------------------------------------------------------------------" )
+    def compute( requestSpec: Dict, rid: str ):
+        try:
+            task: TaskHandle = client.request(requestSpec)
+            result: Optional[TaskResult] = task.getResult(block=True)
+            result_dataset = result.getDataset()
+            output_file = f"{OUTPUT_DIR}/{rid}.nc"
+            try:                          result_dataset.to_netcdf( output_file )
+            except Exception as err0:     print( f"\n\n   &&&&&&&&   ERROR writing result to output file: {output_file}: {err0}   &&&&&&&&   \n\n" )
+        except Exception as err1:
+            print(f"\n\n  &&&&&&&&   Execution ERROR : {err1}   &&&&&&&& ")
+            traceback.print_exc()
 
-operations = [ { "name": "edas.timeResample", "input": "temp", "freq": "1D", "op": "ave,max,min" } ]
-requestSpec = dict( domain=domains, input = variables, operation = operations )
-compute( requestSpec, f"merra2-{base_year}-temp-dailyMinMaxAve" )
+    print( "Compute temperature products ------------------------------------------------------------------------------------------------------------------------" )
 
-
-print( "Compute precipitation products ----------------------------------------------------------------------------------------------------------------------" )
-
-operations = [ { "name": "edas.timeResample", "input": "precip", "freq": "1D", "op": "ave,max,min" } ]
-requestSpec = dict( domain=domains, input = variables, operation = operations )
-compute( requestSpec, f"merra2-{base_year}-precip-dailyMinMaxAve" )
+    operations = [ { "name": "edas.timeResample", "input": "temp", "freq": "1D", "op": "ave,max,min" } ]
+    requestSpec = dict( domain=domains, input = variables, operation = operations )
+    compute( requestSpec, f"merra2-{base_year}-temp-dailyMinMaxAve" )
 
 
-print( "Compute cloud cover products ------------------------------------------------------------------------------------------------------------------------" )
+    print( "Compute precipitation products ----------------------------------------------------------------------------------------------------------------------" )
 
-operations = [ { "name": "edas.timeResample", "input": "clouds", "freq": "1D", "op": "ave,max,min" } ]
-requestSpec = dict( domain=domains, input = variables, operation = operations )
-compute( requestSpec, f"merra2-{base_year}-clouds-dailyMinMaxAve" )
-
-print( "Compute relative humidity products ------------------------------------------------------------------------------------------------------------------" )
-
-operations = [ { "name": "edas.timeResample", "input": "humid", "freq": "1D", "op": "ave,max,min" } ]
-requestSpec = dict( domain=domains, input = variables, operation = operations )
-compute( requestSpec, f"merra2-{base_year}-humid-dailyMinMaxAve" )
+    operations = [ { "name": "edas.timeResample", "input": "precip", "freq": "1D", "op": "ave,max,min" } ]
+    requestSpec = dict( domain=domains, input = variables, operation = operations )
+    compute( requestSpec, f"merra2-{base_year}-precip-dailyMinMaxAve" )
 
 
-print( "Completed processing ------------------------------------------------------------------------------------------------------------------------" )
+    print( "Compute cloud cover products ------------------------------------------------------------------------------------------------------------------------" )
 
+    operations = [ { "name": "edas.timeResample", "input": "clouds", "freq": "1D", "op": "ave,max,min" } ]
+    requestSpec = dict( domain=domains, input = variables, operation = operations )
+    compute( requestSpec, f"merra2-{base_year}-clouds-dailyMinMaxAve" )
+
+    print( "Compute relative humidity products ------------------------------------------------------------------------------------------------------------------" )
+
+    operations = [ { "name": "edas.timeResample", "input": "humid", "freq": "1D", "op": "ave,max,min" } ]
+    requestSpec = dict( domain=domains, input = variables, operation = operations )
+    compute( requestSpec, f"merra2-{base_year}-humid-dailyMinMaxAve" )
+
+    print( "Completed processing ------------------------------------------------------------------------------------------------------------------------" )
 
 
 
